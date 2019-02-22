b160a2c2f8228e9c0a821d5c434df53c6c567351
santiago ventura <santiago.ventura@sap.com>
2019-02-22 00:07:10 -0800

    Fix kyma-gke-end-to-end-test.sh

--- prow/scripts/cluster-integration/kyma-gke-end-to-end-test.sh
+++ prow/scripts/cluster-integration/kyma-gke-end-to-end-test.sh
@@ -207,7 +207,7 @@ kubectl apply -f "${INSTALLER_YAML}"
 "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/generate-cluster-backup-config.sh"
 
 echo "Manual concatenating yamls"
-"${KYMA_SCRIPTS_DIR}"/concat-yamls.sh "${INSTALLER_CR}" \
+"${KYMA_SCRIPTS_DIR}"/concat-yamls.sh "${INSTALLER_CONFIG}" "${INSTALLER_CR}" \
 | sed -e 's;image: eu.gcr.io/kyma-project/.*/installer:.*$;'"image: ${KYMA_INSTALLER_IMAGE};" \
 | sed -e "s/__DOMAIN__/${DOMAIN}/g" \
 | sed -e "s/__REMOTE_ENV_IP__/${REMOTEENVS_IP_ADDRESS}/g" \
